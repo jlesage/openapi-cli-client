fb995d79b84a15810ea4c29a0f3f9d234a54efbd
Jocelyn Le Sage <jocelyn@le-sage.com>
2016-07-14 11:31:00 -0400

    Better integration with Click.
    This also adds the ability to use global parameters that apply to all commands.

--- openapi_cli_client/cli.py
+++ openapi_cli_client/cli.py
@@ -10,7 +10,7 @@ from bravado_core.spec import Spec
 from bravado.client import construct_request
 from bravado.requests_client import RequestsClient
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'], obj={})
 
 REPLACEABLE_COMMAND_CHARS = re.compile('[^a-z0-9]+')
 
@@ -35,7 +35,7 @@ def get_command_name(op):
         return op.http_method
 
 
-def invoke(op, *args, **kwargs):
+def invoke(op, ctx, *args, **kwargs):
     if op.http_method != 'get':
         clickclick.action('Invoking..')
     request = construct_request(op, {}, **kwargs)
@@ -54,30 +54,30 @@ def sanitize_spec(spec):
     return spec
 
 
-def generate_cli(spec):
-    origin_url = None
-    if isinstance(spec, str):
-        if spec.startswith('https://') or spec.startswith('http://'):
-            origin_url = spec
-            r = requests.get(spec)
-            r.raise_for_status()
-            spec = yaml.safe_load(r.text)
-        else:
-            with open(spec, 'rb') as fd:
-                spec = yaml.safe_load(fd.read())
+class OpenAPIClientCLI(click.MultiCommand):
+    def list_commands(self, ctx):
+        commands = []
+        if 'SPEC' in ctx.obj:
+            for res_name, res in ctx.obj['SPEC'].resources.items():
+                commands.append(normalize_command_name(res_name))
 
-    spec = sanitize_spec(spec)
+        return commands
 
-    cli = clickclick.AliasedGroup(context_settings=CONTEXT_SETTINGS)
 
-    spec = Spec.from_dict(spec, origin_url=origin_url)
-    for res_name, res in spec.resources.items():
+    def get_command(self, ctx, name):
+        for res_name, res in ctx.obj['SPEC'].resources.items():
+            if name == normalize_command_name(res_name):
+                return self.gen_command_grp_from_spec(ctx, res_name, res)
+
+        return None
+
+
+    def gen_command_grp_from_spec(self, ctx, res_name, res):
         grp = clickclick.AliasedGroup(normalize_command_name(res_name), short_help='Manage {}'.format(res_name))
-        cli.add_command(grp)
         for op_name, op in res.operations.items():
             name = get_command_name(op)
 
-            cmd = click.Command(name, callback=partial(invoke, op=op), short_help=op.op_spec.get('summary'))
+            cmd = click.Command(name, callback=partial(invoke, op=op, ctx=ctx), short_help=op.op_spec.get('summary'))
             for param_name, param in op.params.items():
                 if param.required:
                     arg = click.Argument([param.name])
@@ -87,14 +87,38 @@ def generate_cli(spec):
                     cmd.params.append(arg)
 
             grp.add_command(cmd)
+        return grp
 
-    return cli
+
+def openapi_spec_callback(ctx, param, value):
+    spec = value
+    origin_url = None
+    if spec.startswith('https://') or spec.startswith('http://'):
+        origin_url = spec
+        r = requests.get(spec)
+        r.raise_for_status()
+        spec = yaml.safe_load(r.text)
+    else:
+        with open(spec, 'rb') as fd:
+            spec = yaml.safe_load(fd.read())
+
+    spec = sanitize_spec(spec)
+    spec = Spec.from_dict(spec, origin_url=origin_url)
+
+    ctx.obj['SPEC'] = spec
+    return value
 
 
-def main():
-    if len(sys.argv) < 2:
-        sys.stderr.write('Missing OpenAPI spec argument\n')
-        sys.exit(1)
-    cli = generate_cli(sys.argv[1])
-    sys.argv = sys.argv[:1] + sys.argv[2:]
-    cli()
+@click.command(cls=OpenAPIClientCLI, context_settings=CONTEXT_SETTINGS)
+@click.argument('OPENAPI_SPEC', callback=openapi_spec_callback)
+@click.pass_context
+def main(ctx, openapi_spec):
+    """
+    Python command line client for REST APIs defined with OpenAPI-Spec/Swagger-Spec.
+
+    The OpenAPI specification is provided by the OPENAPI_SPEC argument and must
+    be in JSON format.  The argument can be the path of a file or an URL where
+    the specification can be fetched.
+    """
+    pass
+
